0b293d4a14d7209412af7a4133baabe2ae34f7d5
ICI <ICI@DESKTOP-EA5M9P6>
2023-05-17 12:38:25 +0900

    게시판 글쓰기 기능 추가

--- src/main/java/com/rubato/home/controller/HomeController.java
+++ src/main/java/com/rubato/home/controller/HomeController.java
@@ -1,10 +1,19 @@
 package com.rubato.home.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.rubato.home.dao.IDao;
+
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private SqlSession sqlSession;
 	
 	@RequestMapping(value = "/index")
 	public String index() {
@@ -24,6 +33,20 @@ public class HomeController {
 	@RequestMapping(value = "/board_view")
 	public String board_view() {
 		return "board_view";
+	}
+	
+	@RequestMapping(value = "/board_writeOk")
+	public String board_writeOk(HttpServletRequest request) {
+		
+		String bname = request.getParameter("bname");
+		String btitle = request.getParameter("btitle");
+		String bcontent = request.getParameter("bcontent");
+		
+		IDao dao = sqlSession.getMapper(IDao.class);
+		
+		dao.boardWriteDao(bname, btitle, bcontent, "정회원", 0);
+		
+		return "redirect:board_list";
 	}
 
 }
---
+++ src/main/java/com/rubato/home/dao/IDao.java
@@ -0,0 +1,7 @@
+package com.rubato.home.dao;
+
+public interface IDao {
+	
+	public int boardWriteDao(String bname, String btitle, String bcontent, String buserid, int filecount);
+	
+}
