e1c9a67d902c1df15086ef682165219cb64c6670
ICI <ICI@DESKTOP-EA5M9P6>
2023-05-17 11:37:25 +0900

    rubato 설정 완료

---
+++ src/main/java/com/rubato/home/RubatoHomeProject20230517Application.java
@@ -0,0 +1,13 @@
+package com.rubato.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubatoHomeProject20230517Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubatoHomeProject20230517Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rubato/home/RubatoHomeProject20230517ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubato.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubatoHomeProject20230517ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
